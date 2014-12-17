c28060666c0cf7246dce23363fb465f507a09fac
Adrien Lecharpentier <adrien.lecharpentier@gmail.com>
2014-12-17 12:46:01 +0100

    Import project

---
+++ src/main/java/fr/lecharpentier/demo/App.java
@@ -0,0 +1,13 @@
+package fr.lecharpentier.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
